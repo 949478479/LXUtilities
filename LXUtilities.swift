54a01f32b1c01a7c06bb46db949f971d5056b0e7
从今以后 <949478479@qq.com>
2017-04-18 00:45:22 +0800

    …

--- LXUtilities.swift
+++ LXUtilities.swift
@@ -7,22 +7,20 @@
 
 import Foundation
 
-func printLog(_ items: Any, file: String = #file, line: Int = #line, function: String = #function) {
+func printLog(_ log: String, file: String = #file, line: Int = #line, function: String = #function) {
     #if DEBUG
-        print("\(function) at \((file as NSString).lastPathComponent)[\(line)] ", items, separator: "")
+        print("\(function) at \((file as NSString).lastPathComponent)[\(line)]", log)
     #endif
 }
 
 func value(from object: Any, forKey key: String) -> Any? {
-    for case let (label?, value) in Mirror(reflecting: object).children.lazy {
-        if label == key {
-            return value
-        }
+    for case let (label?, value) in Mirror(reflecting: object).children.lazy where label == key {
+        return value
     }
     return nil
 }
 
-func synchronized(_ obj: Any!, _ closure: () -> Void) {
+func synchronized(_ obj: Any, _ closure: () -> Void) {
     objc_sync_enter(obj)
     closure()
     objc_sync_exit(obj)
