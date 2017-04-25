a772f6f226af340dfdf3b36366f8c4ede13bec5a
从今以后 <949478479@qq.com>
2017-04-26 00:27:24 +0800

    …

--- Extensions/UIKitExtensions/UIApplication+LXExtension.h
+++ Extensions/UIKitExtensions/UIApplication+LXExtension.h
@@ -2,7 +2,7 @@
 //  UIApplication+LXExtension.h
 //
 //  Created by 从今以后 on 16/2/1.
-//  Copyright © 2016年 apple. All rights reserved.
+//  Copyright © 2016年 从今以后. All rights reserved.
 //
 
 @import UIKit;
@@ -13,6 +13,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 /// `sharedApplication` 的代理。
 + (nullable id<UIApplicationDelegate>)lx_appDelegate;
+
+/// 打开各种系统设置
++ (BOOL)lx_openPrefsWithString:(NSString *)aString;
 
 @end
 
--- Extensions/UIKitExtensions/UIApplication+LXExtension.m
+++ Extensions/UIKitExtensions/UIApplication+LXExtension.m
@@ -2,7 +2,7 @@
 //  UIApplication+LXExtension.m
 //
 //  Created by 从今以后 on 16/2/1.
-//  Copyright © 2016年 apple. All rights reserved.
+//  Copyright © 2016年 从今以后. All rights reserved.
 //
 
 #import "UIApplication+LXExtension.h"
@@ -14,6 +14,17 @@ NS_ASSUME_NONNULL_BEGIN
 + (nullable id<UIApplicationDelegate>)lx_appDelegate
 {
 	return [[self sharedApplication] delegate];
+}
+
++ (BOOL)lx_openPrefsWithString:(NSString *)aString
+{
+    SEL classMethodSel = NSSelectorFromString(@"defaultWorkspace");
+    Class class = NSClassFromString(@"LSApplicationWorkspace");
+    id defaultWorkspace = ((id (*)(id, SEL))[class methodForSelector:classMethodSel])(class, classMethodSel);
+    
+    NSURL *url = [NSURL URLWithString:aString];
+    SEL instanceMethodSel = NSSelectorFromString(@"openSensitiveURL:withOptions:");
+    return ((BOOL (*)(id, SEL, id, id))[defaultWorkspace methodForSelector:instanceMethodSel])(defaultWorkspace, instanceMethodSel, url, nil);
 }
 
 @end
