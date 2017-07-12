1b5f074c2c7b3d701df829e73fe91becda9451c0
从今以后 <949478479@qq.com>
2017-07-13 01:07:38 +0800

    对 lx_openPrefsWithString: 方法使用的字符串做 ASCII 混淆。

--- Extensions/UIKitExtensions/UIApplication+LXExtension.m
+++ Extensions/UIKitExtensions/UIApplication+LXExtension.m
@@ -6,6 +6,7 @@
 //
 
 #import "UIApplication+LXExtension.h"
+#import <objc/runtime.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -17,13 +18,16 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (BOOL)lx_openPrefsWithString:(NSString *)aString
 {
-    SEL classMethodSel = NSSelectorFromString(@"defaultWorkspace");
-    Class class = NSClassFromString(@"LSApplicationWorkspace");
-    id defaultWorkspace = ((id (*)(id, SEL))[class methodForSelector:classMethodSel])(class, classMethodSel);
-    
-    NSURL *url = [NSURL URLWithString:aString];
-    SEL instanceMethodSel = NSSelectorFromString(@"openSensitiveURL:withOptions:");
-    return ((BOOL (*)(id, SEL, id, id))[defaultWorkspace methodForSelector:instanceMethodSel])(defaultWorkspace, instanceMethodSel, url, nil);
+	char className[] = { 0x4c, 0x53, 0x41, 0x70, 0x70, 0x6c, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x57, 0x6f, 0x72, 0x6b, 0x73, 0x70, 0x61, 0x63, 0x65, 0x0 };
+	Class class = objc_lookUpClass(className);
+	char classMethodSelName[] = { 0x64, 0x65, 0x66, 0x61, 0x75, 0x6c, 0x74, 0x57, 0x6f, 0x72, 0x6b, 0x73, 0x70, 0x61, 0x63, 0x65, 0x0 };
+	SEL classMethodSel = sel_registerName(classMethodSelName);
+	id defaultWorkspace = ((id (*)(id, SEL))[class methodForSelector:classMethodSel])(class, classMethodSel);
+
+	char instanceMethodSelName[] = { 0x6f, 0x70, 0x65, 0x6e, 0x53, 0x65, 0x6e, 0x73, 0x69, 0x74, 0x69, 0x76, 0x65, 0x55, 0x52, 0x4c, 0x3a, 0x77, 0x69, 0x74, 0x68, 0x4f, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x73, 0x3a, 0x0 };
+	SEL instanceMethodSel = sel_registerName(instanceMethodSelName);
+	NSURL *url = [NSURL URLWithString:aString];
+	return ((BOOL (*)(id, SEL, id, id))[defaultWorkspace methodForSelector:instanceMethodSel])(defaultWorkspace, instanceMethodSel, url, nil);
 }
 
 @end
