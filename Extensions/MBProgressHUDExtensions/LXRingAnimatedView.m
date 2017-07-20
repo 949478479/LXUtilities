70df1476406a60658b56ecb6f8d517d9e2402406
从今以后 <949478479@qq.com>
2017-07-20 23:42:42 +0800

    改善 LXRingAnimatedView 的动画时间曲线。

--- Extensions/MBProgressHUDExtensions/LXRingAnimatedView.m
+++ Extensions/MBProgressHUDExtensions/LXRingAnimatedView.m
@@ -1,6 +1,5 @@
 //
 //  LXIndefiniteAnimatedView.m
-//  这到底是什么鬼
 //
 //  Created by 从今以后 on 2017/6/5.
 //  Copyright © 2017年 从今以后. All rights reserved.
@@ -95,14 +94,13 @@
 - (void)_addAnimation
 {
     NSTimeInterval duration = self.duration;
-    CAMediaTimingFunction *timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
 
     // end LXMINIMUM => 1
     CABasicAnimation *strokeEnd = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
     strokeEnd.fromValue = @(LXMINIMUM);
     strokeEnd.toValue = @1;
     strokeEnd.duration = duration;
-    strokeEnd.timingFunction = timingFunction;
+    strokeEnd.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
 
     // start 0 => 1 - LXMINIMUM
     CABasicAnimation *strokeStart = [CABasicAnimation animationWithKeyPath:@"strokeStart"];
@@ -111,13 +109,13 @@
     strokeStart.duration = duration;
     strokeStart.beginTime = duration;
     strokeStart.fillMode = kCAFillModeBackwards;
-    strokeStart.timingFunction = timingFunction;
+    strokeStart.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
 
     // 转一周，等于没转
-    CAKeyframeAnimation *rotation = [CAKeyframeAnimation animationWithKeyPath:@"transform.rotation"];
+    CABasicAnimation *rotation = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+	rotation.byValue = @(M_PI * 2);
     rotation.duration = duration * 2;
-    rotation.values = @[@0, @(M_PI), @(2*M_PI)];
-    rotation.timingFunctions = @[timingFunction, timingFunction];
+	rotation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
 
     CAAnimationGroup *animationGroup = [CAAnimationGroup animation];
     animationGroup.delegate = self;
@@ -146,94 +144,5 @@
     
     [self _addAnimation];
 }
-
-/* 这个是旧版实现，不够完美。
-
-- (void)_startAnimation
-{
-    [self _stopAnimation];
-    [self _configureRingLayer];
-
-    uint index = 0;
-    CGFloat radian = M_PI_4 * 3;
-    NSTimeInterval duration = self.duration;
-    NSMutableArray *animations = [NSMutableArray arrayWithCapacity:4];
-
-    CABasicAnimation *strokeEnd = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
-    CABasicAnimation *strokeStart = [CABasicAnimation animationWithKeyPath:@"strokeStart"];
-    CABasicAnimation *rotation = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
-
-    rotation.duration = duration;
-    strokeEnd.duration = duration;
-    strokeStart.duration = duration;
-
-    rotation.fillMode = kCAFillModeForwards;
-    strokeEnd.fillMode = kCAFillModeForwards;
-    strokeStart.fillMode = kCAFillModeForwards;
-
-    CAMediaTimingFunction *timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
-    rotation.timingFunction = timingFunction;
-    strokeEnd.timingFunction = timingFunction;
-    strokeStart.timingFunction = timingFunction;
-
-    // end: LXMINIMUM => 3/4
-
-    rotation.byValue = @(radian);
-    strokeEnd.toValue = @(3 / 4.0);
-    strokeEnd.fromValue = @(LXMINIMUM);
-
-    [animations addObject:rotation.copy];
-    [animations addObject:strokeEnd.copy];
-
-    ++index;
-
-    // start: 0 => 3/4 - LXMINIMUM
-
-    rotation.beginTime = duration * index;
-    strokeStart.beginTime = duration * index;
-
-    rotation.byValue = @(radian);
-    strokeStart.fromValue = @0;
-    strokeStart.toValue = @(3 / 4.0 - LXMINIMUM);
-
-    [animations addObject:rotation.copy];
-    [animations addObject:strokeStart.copy];
-
-    ++index;
-
-    // start: 3/4 - LXMINIMUM => 0
-
-    rotation.beginTime = duration * index;
-    strokeStart.beginTime = duration * index;
-
-    strokeStart.toValue = @0;
-    strokeStart.fromValue = @(3 / 4.0 - LXMINIMUM);
-    rotation.byValue = @(radian + 2 * M_PI * 3 / 4.0);
-
-    [animations addObject:rotation.copy];
-    [animations addObject:strokeStart.copy];
-
-    ++index;
-
-    // end 3/4 => LXMINIMUM
-
-    rotation.beginTime = duration * index;
-    strokeEnd.beginTime = duration * index;
-
-    strokeEnd.fromValue = @(3 / 4.0);
-    strokeEnd.toValue = @(LXMINIMUM);
-    rotation.byValue = @(radian + 2 * M_PI * 3 / 4.0);
-
-    [animations addObject:rotation.copy];
-    [animations addObject:strokeEnd.copy];
-
-    CAAnimationGroup *animationGroup = [CAAnimationGroup animation];
-    animationGroup.repeatCount = INFINITY;
-    animationGroup.duration = duration * 4;
-    animationGroup.animations = animations;
-
-    [self.ringLayer addAnimation:animationGroup forKey:nil];
-}
-*/
 
 @end
