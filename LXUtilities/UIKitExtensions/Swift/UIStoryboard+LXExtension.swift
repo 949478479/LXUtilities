67a2a713fdf33f50ac673891c6e43b28482f9a40
从今以后 <949478479@qq.com>
2017-10-19 16:06:23 +0800

    更新.

--- LXUtilities/UIKitExtensions/Swift/UIStoryboard+LXExtension.swift
+++ LXUtilities/UIKitExtensions/Swift/UIStoryboard+LXExtension.swift
@@ -10,6 +10,21 @@ import UIKit
 
 extension Swifty where Base: UIStoryboard {
 
+    /// 实例化指定故事版中的初始控制器.
+    static func instantiateInitialViewController<T: UIViewController>(withStoryboardName name: String) -> T {
+        return Base(name: name, bundle: nil).instantiateInitialViewController() as! T
+    }
+
+    /// 以类名作为标识符实例化指定故事版中的初始控制器.
+    static func instantiateViewController<T: UIViewController>(withStoryboardName name: String, for type: T.Type) -> T {
+        return Base(name: name, bundle: nil).instantiateViewController(withIdentifier: String(describing: type)) as! T
+    }
+
+    /// 实例化故事版中的初始控制器.
+    func instantiateInitialViewController<T: UIViewController>() -> T {
+        return base.instantiateInitialViewController() as! T
+    }
+
     /// 以类名作为标识符实例化视图控制器.
     func instantiateViewController<T: UIViewController>(for type: T.Type) -> T {
         return base.instantiateViewController(withIdentifier: String(describing: type)) as! T
