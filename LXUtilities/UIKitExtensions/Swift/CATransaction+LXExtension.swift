53b2c20cd8a9a83684c2366213e4bc10dc7eeb1d
从今以后 <949478479@qq.com>
2018-05-24 09:16:21 +0800

    完善 CATransaction 扩展

--- LXUtilities/UIKitExtensions/Swift/CATransaction+LXExtension.swift
+++ LXUtilities/UIKitExtensions/Swift/CATransaction+LXExtension.swift
@@ -10,9 +10,9 @@ import QuartzCore
 extension Swifty where Base: CATransaction {
 
     /**
-     禁用隐式动画。
+     开启事务，在事务中禁用隐式动画。
 
-     - parameter actionsWithoutAnimation: 在闭包内修改图层属性不会触发隐式动画
+     - parameter actionsWithoutAnimation: 在闭包内修改图层属性不会触发隐式动画。
      */
     static func performWithoutAnimation(_ actionsWithoutAnimation: () -> ()) {
         Base.begin()
@@ -22,18 +22,21 @@ extension Swifty where Base: CATransaction {
     }
 
     /**
-     定制隐式动画。
+     开启事务，并在其中添加隐式和显式的图层动画。
 
-     - parameter duration:   隐式动画持续时间。
-     - parameter animations: 在此闭包内修改图层属性。
-     - parameter completion: 隐式动画完成时调用此闭包。
+     - parameter duration:   隐式动画持续时间，若显式动画未设置动画时间，则会使用该值。
+     - parameter animations: 在闭包中修改图层属性或添加图层动画。
+     - parameter completion: 此闭包会在事务中的全部动画均完成时调用。
      */
-    static func animateWithDuration(_ duration: CFTimeInterval, animations: () -> (),
-        completion: (() -> ())? = nil) {
+    static func animateWithDuration(
+        _ duration: CFTimeInterval,
+        animations: () -> (),
+        completion: (() -> ())? = nil)
+    {
         Base.begin()
         Base.setAnimationDuration(duration)
         Base.setCompletionBlock(completion)
-        animations();
+        animations()
         Base.commit()
     }
 }
